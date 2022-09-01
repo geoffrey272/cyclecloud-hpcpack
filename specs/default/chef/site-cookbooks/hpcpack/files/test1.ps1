b96345c1a77c79564d31d8b8cf9f361e5c1fae1c
t-jiaxiangli <t-jiaxiangli@microsoft.com>
2022-09-01 16:29:51 +0800

    version1

--- specs/default/chef/site-cookbooks/hpcpack/files/test1.ps1
+++
@@ -1,8 +0,0 @@
-$created = $null
-$firstHeadNode = "HAHN01"
-while($null -eq $created){
-    $remoteKeys = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey("LocalMachine", $firstHeadNode)
-    $remoteKey = $remoteKeys.opensubkey("SOFTWARE\Microsoft\HPC")
-    $created = $remoteKey.getValue("created")
-    wirte-host "wait ...."
-}
